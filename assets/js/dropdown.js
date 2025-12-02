2a2dee389d34a1a92e80308defac85a06556ac33
andreredesti <andreredesti@gmail.com>
2025-12-01 23:40:45 -0300

    Novos ajustes nos links de navegação. 01/12/2025

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,3 +1,22 @@
+ 
+ const dropdownBtn = document.querySelector('.dropbtn');
+        const dropdownContent = document.getElementById("myDropdown");
+    
+        dropdownBtn.addEventListener('click', function(){
+            dropdownContent.classList.toggle("show");
+        });
+
+        window.onclick = function(event) {
+            const dropdowns = document.getElementsByClassName('dropdown-content');
+            if (!event.target.matches(".dropbtn")) {
+                const dropdowns = document.getElementsByClassName("dropdown-content");
+           if (dropdowns[0].classList.contains('show')) {
+                dropdowns[0].classList.remove('show');
+            }
+        }
+    }
+
+/*
 document.querySelectorAll('.toggle-btn').forEach(btn => {
     btn.addEventListener('click', () => {
         const divId = btn.dataset.target;
@@ -12,3 +31,5 @@ document.querySelectorAll('.toggle-btn').forEach(btn => {
         }
     });
 });
+
+*/
