3d7efef3baec96724c97c7b6e69325c06ce9ed10
andreredesti <andreredesti@gmail.com>
2025-12-02 09:25:32 -0300

    Novos ajustes nos links de navegação. 02/12/2025

--- assets/js/toggle_btn.js
+++ assets/js/toggle_btn.js
@@ -3,15 +3,24 @@ document.querySelectorAll(".toggle-btn").forEach(btn => {
     btn.addEventListener('click', () => {
         const divId = btn.dataset.target;
         const div = document.getElementById(divId);
+       // const button = document.classList('mostrarEsconder');
 
         div.classList.toggle('hidden');
 
-        if(div.classList.contains('hidden')) {
-            btn.textContent = 'Esconder';
-            div.style.display = "none"
+        if (div.classList.contains('hidden')) {
+            btn.textContent = 'Mostrar';
+            // div.style.color = "red"
+            // div.style.display = "none";
+            //button.style.backgroundColor = "blue"
+
+            //} //else if (div.classList.contains('!hidden')){
+            //  console.log("eeesttt");
+
         } else {
-            btn.textContent = "Mostrar";
-            div.style.display = "block";
+            btn.textContent = "Esconder";
+            // div.style.display = "block";
+            // div.style.background = "btn-danger"
+             //button.style.backgroundColor = "red"
         }
     });
 });
