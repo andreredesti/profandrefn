44348ecef6ffe669275367b3c8d39af8dbc34095
andreredesti <andreredesti@gmail.com>
2025-12-29 21:21:19 -0300

    Atualização do conteúdo. 29/12/2025

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -16,20 +16,3 @@
         }
     }
 
-/*
-document.querySelectorAll('.toggle-btn').forEach(btn => {
-    btn.addEventListener('click', () => {
-        const divId = btn.dataset.target;
-        const div = document.getElementById(divId);
-
-        div.classList.toggle('hidden');
-
-        if (div.classList.contains('hidden')) {
-            btn.textContent = "Mostrar";
-        } else {
-            btn.textContent = "Esconder";
-        }
-    });
-});
-
-*/
--- assets/js/toggle_btn.js
+++ assets/js/toggle_btn.js
@@ -3,24 +3,14 @@ document.querySelectorAll(".toggle-btn").forEach(btn => {
     btn.addEventListener('click', () => {
         const divId = btn.dataset.target;
         const div = document.getElementById(divId);
-       // const button = document.classList('mostrarEsconder');
-
+    
         div.classList.toggle('hidden');
 
         if (div.classList.contains('hidden')) {
             btn.textContent = 'Mostrar';
-            // div.style.color = "red"
-            // div.style.display = "none";
-            //button.style.backgroundColor = "blue"
-
-            //} //else if (div.classList.contains('!hidden')){
-            //  console.log("eeesttt");
 
         } else {
             btn.textContent = "Esconder";
-            // div.style.display = "block";
-            // div.style.background = "btn-danger"
-             //button.style.backgroundColor = "red"
         }
     });
 });
